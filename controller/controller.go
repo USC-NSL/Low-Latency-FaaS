da47e599963fc1511c6a673a3028d9f6d814356a
Jianfeng Wang <pkueewjf@gmail.com>
2020-04-05 17:31:14 -0700

    Add a NF DAG input

--- controller/controller.go
+++ controller/controller.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
 
 	grpc "github.com/USC-NSL/Low-Latency-FaaS/grpc"
 )
@@ -11,17 +13,19 @@ import (
 // |ToRGRPCHandler| are functions to handle gRPC requests to the ToR switch.
 // |workers| are all the worker nodes (i.e. physical or virtual machines) in the system.
 // |instances| maintains all running NF instances.
+// |dags| maintains all logical representations of NF DAGs.
 type FaaSController struct {
 	grpc.ToRGRPCHandler
-	workers   map[string]*Worker
-	instances map[string]Instance
+	workers map[string]*Worker
+	// instances map[string]Instance
+	dags map[string]*DAG
 }
 
 // Creates a new FaaS controller.
 func NewFaaSController() *FaaSController {
 	c := &FaaSController{
-		workers:   make(map[string]*Worker),
-		instances: make(map[string]Instance),
+		workers: make(map[string]*Worker),
+		dags:    make(map[string]*DAG),
 	}
 
 	// Initializes all worker nodes when starting a |FaaSController|.
@@ -62,8 +66,39 @@ func (c *FaaSController) GetWorkerInfoByName(nodeName string) string {
 	return ""
 }
 
-func (c *FaaSController) ConnectFunctions(upNF string, downNF string) error {
-	return nil
+func (c *FaaSController) AddNF(user string, funcType string) error {
+	if _, exists := c.dags[user]; !exists {
+		c.dags[user] = newDAG()
+	}
+
+	return c.dags[user].addNF(funcType)
+}
+
+func (c *FaaSController) ConnectNFs(user string, upNF string, downNF string) error {
+	if _, exists := c.dags[user]; !exists {
+		c.dags[user] = newDAG()
+	}
+
+	return c.dags[user].connectNFs(upNF, downNF)
+}
+
+func (c *FaaSController) ShowNFDAGs(targetUser string) {
+	for user, dag := range c.dags {
+		if user == targetUser || targetUser == "all" {
+			fmt.Printf("[%s] deploys NF DAG:\n", user)
+
+			// Prints the NF graphs to the terminal.
+			drawCmd := exec.Command("graph-easy")
+			drawIn, _ := drawCmd.StdinPipe()
+			drawOut, _ := drawCmd.StdoutPipe()
+			drawCmd.Start()
+			drawIn.Write([]byte(dag.String()))
+			drawIn.Close()
+			drawBytes, _ := ioutil.ReadAll(drawOut)
+			fmt.Println(string(drawBytes))
+			drawCmd.Wait()
+		}
+	}
 }
 
 func (c *FaaSController) CreateInstance(nodeName string, funcType string) error {
