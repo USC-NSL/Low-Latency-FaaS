5d1c0cf3dd4836358433bf7a88461aa5556c6c9e
Jianfeng Wang <pkueewjf@gmail.com>
2020-05-01 17:32:11 -0700

    Check the CLI input length

--- cli/executor.go
+++ cli/executor.go
@@ -68,31 +68,31 @@ func (e *Executor) Execute(s string) {
 		} else {
 			fmt.Printf(e.FaaSController.GetWorkersInfo())
 		}
-	} else if words[0] == "add" && len(words) > 3 {
+	} else if words[0] == "add" && len(words) >= 3 {
 		nodeName := words[1]
 		nfs := words[2:]
 
 		e.FaaSController.CreateSGroup(nodeName, nfs)
-	} else if words[0] == "rm" && len(words) > 2 {
+	} else if words[0] == "rm" && len(words) >= 3 {
 		nodeName := words[1]
 		groupID, _ := strconv.Atoi(words[2])
 
 		e.FaaSController.DestroySGroup(nodeName, groupID)
-	} else if words[0] == "attach" && len(words) > 2 {
+	} else if words[0] == "attach" && len(words) >= 4 {
 		nodeName := words[1]
 		groupId, _ := strconv.Atoi(words[2])
 		coreId, _ := strconv.Atoi(words[3])
 		if err := e.FaaSController.AttachSGroup(nodeName, groupId, coreId); err != nil {
 			fmt.Printf("Failed to attach sGroup (id=%d) on core %d of worker %s: %s!\n", groupId, coreId, nodeName, err.Error())
 		}
-	} else if words[0] == "detach" && len(words) > 2 {
+	} else if words[0] == "detach" && len(words) >= 3 {
 		nodeName := words[1]
 		groupId, _ := strconv.Atoi(words[2])
 
 		if err := e.FaaSController.DetachSGroup(nodeName, groupId); err != nil {
 			fmt.Printf("Failed to detach sGroup (id=%d) on worker %s: %s!\n", groupId, nodeName, err.Error())
 		}
-	} else if words[0] == "kubectl" && len(words) > 2 {
+	} else if words[0] == "kubectl" && len(words) >= 3 {
 		command := words[1]
 		deploymentName := words[2]
 		if command == "rm" {
@@ -102,7 +102,7 @@ func (e *Executor) Execute(s string) {
 				fmt.Printf("Remove deployment %s successfully!\n", deploymentName)
 			}
 		}
-	} else if words[0] == "flow" && len(words) > 5 {
+	} else if words[0] == "flow" && len(words) >= 6 {
 		srcIp := words[1]
 		srcPort, _ := strconv.Atoi(words[2])
 		dstIp := words[3]
@@ -113,13 +113,13 @@ func (e *Executor) Execute(s string) {
 		} else {
 			fmt.Printf("Return dmac = %s.", dmac)
 		}
-	} else if words[0] == "deploy" && len(words) > 2 {
+	} else if words[0] == "deploy" && len(words) >= 3 {
 		user := words[1]
 		funcType := words[2]
 		if err := e.FaaSController.AddNF(user, funcType); err != nil {
 			fmt.Printf("Failed to add SGroup [%s]: %s.\n", funcType, err.Error())
 		}
-	} else if words[0] == "connect" && len(words) > 3 {
+	} else if words[0] == "connect" && len(words) >= 4 {
 		user := words[1]
 		up := words[2]
 		down := words[3]
@@ -128,10 +128,10 @@ func (e *Executor) Execute(s string) {
 		}
 
 		e.FaaSController.ShowNFDAGs(user)
-	} else if words[0] == "show" && len(words) > 1 {
+	} else if words[0] == "show" && len(words) >= 2 {
 		user := words[1]
 		e.FaaSController.ShowNFDAGs(user)
-	} else if words[0] == "activate" && len(words) > 1 {
+	} else if words[0] == "activate" && len(words) >= 2 {
 		user := words[1]
 		e.FaaSController.ActivateDAG(user)
 	}
